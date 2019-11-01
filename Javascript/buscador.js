4e5d876278b15e0a76a94582c7347acb919eaf7f
milihancevic <mhancevic@udesa.edu.ar>
2019-11-01 15:04:06 -0300

    :)

---
+++ Javascript/buscador.js
@@ -0,0 +1,27 @@
+// var queryString= location.search;
+// var searchParams= new URLSearchParams (queryString);
+// var search= search.Params.get("search");
+
+//var url=.....
+// en la url le agregamos con + + la variable que tiene la infomacion que en este caso es search
+
+//fetch(url)
+// .then(function(respuesta){
+// return respuesta.json();
+
+//})
+//.then(function(datos){
+// var destino= document.querySelector(".resultados");
+//  var datosFinales= datos.data;
+// var li= "" ; (lo que hacemos con el for es llenar esta variable que esta vacia)
+
+// for(var i=0; i<datosFinales.lenght; i++){
+// li += "<li>"
+// li += datosFinales[i].images.original.url
+// li+= "</li>"
+// destino.innerHTML = li;
+// }
+
+
+// console.log(datos)
+//})
