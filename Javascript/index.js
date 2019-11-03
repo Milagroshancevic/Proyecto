f7814e542a79eefd5f2287f520275079c8fcaed4
milihancevic <mhancevic@udesa.edu.ar>
2019-11-03 15:36:00 -0300

    fitch

---
+++ Javascript/index.js
@@ -0,0 +1,153 @@
+window.onload = function () {
+//Al aire hoy//
+  fetch ("https://api.themoviedb.org/3/tv/airing_today?api_key=4c34fda4463cc4b5610955320cdc1b52&language=en-US")
+  .then(function(respuesta){
+    return respuesta.json();
+  })
+  .then(function(datos){
+    var seriesAire = datos.results;
+    var listaire= document.querySelector(".listaire");
+    //console.log(seriesAire);
+
+    for(var i=0; i<seriesAire.length; i++) {
+      listaire.innerHTML += '<p>' + seriesAire[i].name + '</p>'
+    }
+
+    var posters= datos.results;
+    var posteraire = document.querySelector(".posteraire");
+
+    for (var i = 0; i < seriesAire.length; i++) {
+      posteraire.innerHTML += '<img src="' + "https://image.tmdb.org/t/p/original/" + datos.results[i].backdrop_path + '">' 
+    }
+
+
+      //console.log(seriesAire[i].name)
+
+
+
+
+
+
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
