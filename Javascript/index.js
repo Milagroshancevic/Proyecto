430b2849950fc524482cf6f8d31861c9f40e922c
belugarciablad <mgarciablad@udesa.edu.ar>
2019-11-03 16:49:34 -0300

    mas puntuadas

--- Javascript/index.js
+++ Javascript/index.js
@@ -13,22 +13,20 @@ window.onload = function () {
       listaire.innerHTML += '<p>' + seriesAire[i].name + '</p>' + '<img src="' + "https://image.tmdb.org/t/p/original/" + datos.results[i].backdrop_path + '">'
     }
 
-    // var posters= datos.results;
-    // var posteraire = document.querySelector(".posteraire");
-    //
-    // for (var i = 0; i < seriesAire.length; i++) {
-    //   listaire.innerHTML += '<img src="' + "https://image.tmdb.org/t/p/original/" + datos.results[i].backdrop_path + '">'
-    // }
+    var posters= datos.results;
+    var posteraire = document.querySelector(".posteraire");
+
+    for (var i = 0; i < seriesAire.length; i++) {
+      posteraire.innerHTML += '<img src="' + "https://image.tmdb.org/t/p/original/" + datos.results[i].backdrop_path + '">'
+    }
 
 
       //console.log(seriesAire[i].name)
 
+
   })
 
-
-// mejores puntuadas
-
-fetch ("https://api.themoviedb.org/3/tv/top_rated?api_key=4c34fda4463cc4b5610955320cdc1b52&page=1")
+  fetch ("https://api.themoviedb.org/3/tv/top_rated?api_key=4c34fda4463cc4b5610955320cdc1b52&page=1")
 .then(function(respuestaMP){
   return respuestaMP.json();
 })
@@ -67,19 +65,10 @@ fetch ("https://api.themoviedb.org/3/tv/top_rated?api_key=4c34fda4463cc4b5610955
 
 
 
-fetch ("https://api.themoviedb.org/3/tv/popular?api_key=4c34fda4463cc4b5610955320cdc1b52&page=1")
-.then(function(respuestaP){
-return respuestaP.json();
-})
-.then(function(datosP){
-  var seriesP= datosP.results;
-  var listapopu= document.querySelector(".listapopu");
 
-  for(var i=0; i<seriesP.lenght;i++){
-    listapopu.innerHTML += '<p>' + seriesP[i].name + '</p>' + '<img src="' + "https://image.tmdb.org/t/p/original/" + datosP.results[i].backdrop_path + '">'
 
-  }
-})
+
+
 
 
 
