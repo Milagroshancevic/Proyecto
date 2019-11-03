8dc536c38dce5081391801087b56f0442755ba78
belugarciablad <mgarciablad@udesa.edu.ar>
2019-11-03 16:11:20 -0300

    Auto stash before merge of "master" and "origin/master"

--- Javascript/index.js
+++ Javascript/index.js
@@ -13,6 +13,23 @@ window.onload = function () {
       listaire.innerHTML += '<p>' + seriesAire[i].name + '</p>' + '<img src="' + "https://image.tmdb.org/t/p/original/" + datos.results[i].backdrop_path + '">'
     }
 
+    var posters= datos.results;
+    var posteraire = document.querySelector(".posteraire");
+
+    for (var i = 0; i < seriesAire.length; i++) {
+      posteraire.innerHTML += '<img src="' + "https://image.tmdb.org/t/p/original/" + datos.results[i].backdrop_path + '">' 
+    }
+
+
+      //console.log(seriesAire[i].name)
+
+
+
+
+
+
+
+
   })
 
   
